31823cd5cfcfda6ce97cb6bb83a85d2acc7b2a31
Thiago Dias da Silva <silvathiago011@hotmail.com>
2023-09-04 20:47:22 -0300

    finalized challenge meteorite program

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-# Use an official Java Development Kit (JDK) image as the base
-FROM openjdk:11-jdk-slim
-
-# Set the working directory inside the container
-WORKDIR /app
--- Test-project/src/App.java
+++ Test-project/src/App.java
@@ -1,19 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        //Stack<Integer> meteoritesFromEachFarm = new Stack<Integer>();
-        
         //variaveis do scopo geral
         int meteorites, tests=0, out=0, coords [] = new int [2];
+        //variavel array com chave, hashmap
+        Map<Integer, Integer> fell = new HashMap<>();
         //enquanto o usuario não indicar as coordenadas de saida.
         while (out == 0) {
             //pergunto os limits da fazenda para o usuario e declaro o vetor limites da fazenda.
             System.out.println("Now type it exactly four coords that delimits your farm. The limits for this are between 0 and 10000. The first X coord need to higher than second X coord and the first Y coord need to Smaller than second Y coord");
             int  limits_of_farm [] = new int [4];
             
-            //um for para preencher o array area da fazenda e ja verificar se os limits x e y estão de acordo com as restrições e se o usuario quer finalizar a entrada.
+            //um FOR para preencher o array area da fazenda e ja verificar se os limits x e y estão de acordo com as restrições e se o usuario quer finalizar a entrada.
             for (int i = 0; i < limits_of_farm.length; i++) {
                 limits_of_farm[i] = sc.nextInt();
                 if(limits_of_farm[0] < limits_of_farm[2]){
@@ -36,9 +38,11 @@ public class App {
             //se o usuario indicar a saida 
             if (out == 1) {
                 System.out.println("Final Program! you can see the responses bellow, thank you!");
+
             //caso o usuario continue, ele ira a seguir dizer a quantidade de meteoritos que cairam na fazenda e digitar as coordenadas x e y de cada meteorito.
             }else{
-                int meteoriteInFarm = 0;
+                //variaveis locais para identificar o test que está e quantos meteoritos cairam na fazenda, apos isso à entrada de dados de quantos cairam e as coords.
+                int falling=0;
                 tests++;
                 System.out.println("Now we need to know how many meteorites fell in your farm");
                 meteorites = sc.nextInt();
@@ -51,19 +55,20 @@ public class App {
                     }
                     //aqui faço o calculo para saber se o meteorito está dentro da fazenda, caso sim contabilizo uma soma para saber quantos ao todo cairam na fazendo do teste atual. 
                     if(((limits_of_farm[0] + limits_of_farm[2]) / 2) > coords[0] && ((limits_of_farm[1] + limits_of_farm[3]) / 2) > coords[1]){
-                        meteoriteInFarm++;
+                        falling++;
+                        fell.put(tests, falling);
                     }
                 }
-                //meteoritesFromEachFarm.push(meteoriteInFarm);
             }
         }
         //mostrar resultado dos testes feitos.
         for (int i = 1; i <= tests; i++) {
             System.out.println("test " + i);
-            //System.out.println(meteoritesFromEachFarm[i]);
-            
+            System.out.println(fell.get(i));
         }
+        sc.close();
     }
+
 }
 
 
